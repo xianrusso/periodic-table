031de326b7d2edee7488c80c8c4ef2f14c81083f
xian <iamchristianrusso@gmail.com>
2019-04-16 22:28:39 -0400

    add structure of project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Element from "./Element";
+
+document.title = 'The Periodic Table';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>The Periodic Table</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Element.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+
+import { elements } from "./_data"
+
+export default class Element extends Component {
+  
+  render() {
+    let { num } = this.props;
+    let element = elements[num];
+    return (
+      <div
+        className={`element element-${num} ${element.category}`}
+      >
+        <div className="number">{element.number}</div>
+        <div className="symbol">{element.symbol}</div>
+        <div className="element-name">{element.name}</div>
+      </div>
+    )
+  }
+}
