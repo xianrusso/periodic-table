b1185d0219208824afb1c055ece65a6e07e5b542
xian <iamchristianrusso@gmail.com>
2024-09-19 14:31:32 -0400

    Added typeface and increased the w/h of the elems so that the longer names (i.e., Rutherfordium) aren't so scrunched.  Still needs work, but looks better than before!

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -7,9 +7,7 @@ export default class Element extends Component {
     let { num } = this.props;
     let element = elements[num];
     return (
-      <div
-        className={`element element-${num} ${element.category}`}
-      >
+      <div className={`element element-${num} ${element.category}`}>
         <div className="number">{element.number}</div>
         <div className="symbol">{element.symbol}</div>
         <div className="element-name">{element.name}</div>
